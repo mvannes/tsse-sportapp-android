971960e392fdee10ab02a3f6d52be40fed028c76
Mitchell de Vries <mdvries1@live.nl>
2017-03-08 19:11:13 +0100

    Added onclick navigation

--- app/src/main/java/sport/tsse/com/sportapp/MainActivity.kt
+++ app/src/main/java/sport/tsse/com/sportapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package sport.tsse.com.sportapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_get_started.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_forgot_password)
+        setContentView(R.layout.fragment_get_started)
+
+        login_text_view.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
