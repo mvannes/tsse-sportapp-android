eeb2d3c228941ebfcca663b2de57603c24af76b7
Mitchell de Vries <mdvries1@live.nl>
2017-03-08 16:08:39 +0100

    Design for forgot password

--- app/src/main/java/sport/tsse/com/sportapp/ForgotPasswordActivity.kt
+++ app/src/main/java/sport/tsse/com/sportapp/ForgotPasswordActivity.kt
@@ -3,10 +3,12 @@ package sport.tsse.com.sportapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_forgot_password)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false);
     }
 }
