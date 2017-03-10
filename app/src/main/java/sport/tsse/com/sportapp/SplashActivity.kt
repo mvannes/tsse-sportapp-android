7c71f2394958dc0b3d638146aaf1da9996a9da44
Mitchell de Vries <mdvries1@live.nl>
2017-03-10 17:57:31 +0100

    Added splashscreen to the flow of the app

---
+++ app/src/main/java/sport/tsse/com/sportapp/SplashActivity.kt
@@ -0,0 +1,24 @@
+package sport.tsse.com.sportapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import sport.tsse.com.sportapp.onboarding.register.RegisterActivity
+
+/**
+ * tsse-sportapp-android
+ *
+ * @author Mitchell de Vries
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, StartupActivity::class.java))
+            finish()
+        }, 1200)
+    }
+}
