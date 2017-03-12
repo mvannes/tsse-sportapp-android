471563b4c11467800ad16620fb1cf9c28b9a21b6
Mitchell de Vries <mdvries1@live.nl>
2017-03-12 20:23:04 +0100

    Added some color changes, design for exercise detail

---
+++ app/src/main/java/sport/tsse/com/sportapp/exercise/ExerciseDetailActivity.kt
@@ -0,0 +1,20 @@
+package sport.tsse.com.sportapp.exercise
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_exercise_detail.*
+import sport.tsse.com.sportapp.R
+
+class ExerciseDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_exercise_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(false)
+        }
+    }
+}
--- app/src/main/java/sport/tsse/com/sportapp/onboarding/login/LoginFragment.kt
+++ app/src/main/java/sport/tsse/com/sportapp/onboarding/login/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_login.*
+import sport.tsse.com.sportapp.exercise.ExerciseDetailActivity
 import sport.tsse.com.sportapp.R
 import sport.tsse.com.sportapp.onboarding.forgotPassword.ForgotPasswordActivity
 
@@ -23,6 +24,10 @@ class LoginFragment : Fragment() {
 
         having_trouble.setOnClickListener {
             startActivity(Intent(activity, ForgotPasswordActivity::class.java))
+        }
+
+        loginButton.setOnClickListener {
+            startActivity(Intent(activity, ExerciseDetailActivity::class.java))
         }
     }
 }
