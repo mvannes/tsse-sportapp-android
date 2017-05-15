e0572c8886b2c7a4793ea11ff11fabb2021a112f
Mitchell de Vries <mdvries1@live.nl>
2017-05-15 16:39:11 +0200

    Added workoutlist

---
+++ app/src/main/java/sport/tsse/com/sportapp/workout/list/WorkoutListView.kt
@@ -0,0 +1,16 @@
+package sport.tsse.com.sportapp.workout.list
+
+import sport.tsse.com.sportapp.base.BaseView
+import sport.tsse.com.sportapp.data.Exercise
+import sport.tsse.com.sportapp.data.Workout
+
+/**
+ * tsse-sportapp-android
+ *
+ * @author Mitchell de Vries
+ */
+interface WorkoutListView : BaseView {
+
+   fun populateView(workouts: List<Workout>)
+
+}
